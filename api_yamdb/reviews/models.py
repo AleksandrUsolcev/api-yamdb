e849f50f89e7ce9880f9b868bd03d2a0d93340ee
Александр Усольцев <usolev@yandex.ru>
2022-07-25 21:25:26 +0300

    update user model

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    roles = (
+        ('admin', 'Администратор'),
+        ('moderator', 'Модератор'),
+        ('user', 'Аутентифицированный пользователь')
+    )
+    role = models.CharField(
+        'Роль',
+        choices=roles,
+        max_length=10
+    )
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+        max_length=300
+    )
