c33e06bec16b484dda57287b9c95e633c4ea989d
Александр Усольцев <usolev@yandex.ru>
2022-07-27 01:33:27 +0300

    add signup serializer and viewset

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,14 @@
-from django.urls import include, path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from . import views
 
 app_name = 'api'
 
-urlpatterns = [
+router = DefaultRouter()
 
+router.register('auth/signup', views.UserSignupViewSet, basename='signup')
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
 ]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,1 +1,16 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework import viewsets
+
+from reviews.models import User
+from .serializers import UserSignupSerializer
+
+
+class UserSignupViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSignupSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def perform_create(self, serializer):
+        email = serializer.data['email']
+        username = serializer.data['username']
+        user = User.objects.create_user(username=username, email=email)
+        user.save()
