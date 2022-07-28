cc88689adcabd8f78c7ff09fc6393b2955175e7a
Aleksei Viunik <vjunik88@gmail.com>
2022-07-28 21:06:46 +0300

    urls for reviews and comments have been added

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -7,6 +7,16 @@ app_name = 'api'
 
 v1 = DefaultRouter()
 v1.register('titles', views.TitleViewSet, basename='titles')
+v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    views.ReviewViewSet,
+    basename='reviews'
+)
+v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'r'/comments',
+    views.CommentViewSet,
+    basename='comments'
+)
 v1.register('categories', views.CategoryViewSet, basename='categories')
 v1.register('genres', views.GenreViewSet, basename='genres')
 v1.register('users', views.UsersViewSet, basename='users')
